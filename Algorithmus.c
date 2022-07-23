3307c6841683ea121cde504319d9266bf1babcd4
lennard98 <lennard_schneider@web.de>
2022-07-23 15:56:09 +0200

    switch case Schalter wurden hinzugefügt

--- Algorithmus.c
+++ Algorithmus.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
-
-int main()
+int main(int argc, char* argv[])
 {
+
+	if ( argc <2)
+	{
+		printf("Bitte -h nutzen \n");
+		return 0;
+	}
+
 	int Dezimalzahl;
-	printf("Bitte einen Integer zwischen 0 und 65534 eingeben:\n");
-	scanf("%d",&Dezimalzahl);
+	int c;
 
-	if (Dezimalzahl < 65535)
-	{
-		printf("Eingegebene Zahl ist: %d \n",Dezimalzahl);
+	while ((c = getopt (argc, argv, "hf1:"))!= 1)
+		switch (c)
+		{
+		case 'h': 
+			printf("Um Zahlen einzugeben nutzen Sie bitte den Schalter -f. Sie bekommen als Rückgabe die Zahl als 4 stelligen Char.. \n");
+			exit(0);
 
-	}	
+		case 'f':
+			printf("Bitte eine Zahl von 0 bis maximal 65534 eingeben.\n");
+			scanf("%d",&Dezimalzahl);
+		
+
+			if (Dezimalzahl < 65535)
+			{		
+				printf("Eingegebene Zahl ist: %d \n",Dezimalzahl);
+
+			}	
 	
-	else
-	{	
-		printf("Bitte noch einmal versuchen es sind nur Zahlen KLEINER 65535 erlaubt.\n");
+			else
+			{	
+				printf("Bitte noch einmal versuchen es sind nur Zahlen KLEINER 65535 erlaubt.\n");
 	
-		scanf("%d",&Dezimalzahl);
-	}
+				scanf("%d",&Dezimalzahl);
+			}
 
-	long Quotient, Rest;
-	int i, j=0;
-	char Hexadezimal[3];
+			long Quotient, Rest;
+			int i, j=0;
+			char Hexadezimal[4];
 
-	Quotient = Dezimalzahl;
+			Quotient = Dezimalzahl;
 
-	while (Quotient !=0)
+			while (Quotient !=0)
 
-	{
-		Rest = Quotient % 16;
-		if (Rest<10)
-			Hexadezimal[j++]= 48 + Rest;
-		else
-			Hexadezimal[j++]= 55 + Rest;
-			Quotient = Quotient / 16;
-	}
+			{
+				Rest = Quotient % 16;
+				if (Rest<10)
+					Hexadezimal[j++]= 48 + Rest;
+				else
+					Hexadezimal[j++]= 55 + Rest;
+					Quotient = Quotient / 16;
+			}
 
-	//Zeige Integer als Char
-	printf("Der Integer als Char ist:\n");
+			//Zeige Integer als Char
+			printf("Der Integer als Char ist:\n");
 
-	for (i = j; i>=0; i--)
-	{
-		printf("%c",Hexadezimal[i]);
-	}
-	printf("\n");	
-	return 0;
+			for (i = j; i>=0; i--)
+			{
+				printf("%c",Hexadezimal[i]);
+			}
+				printf("\n");	
+		
+				return 0;
+		case '?':
+				printf("Bitte -h benutzen!\n");
+				 exit(1);
+		
+		}
 }
